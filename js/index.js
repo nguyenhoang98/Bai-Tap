70a46af212e66e8b123908802d2b2964bab62a16
nguyenhoang98 <nguyenvanhoangg150998@gmail.com>
2021-03-11 09:52:03 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.getElementById("action").onclick = function () {
+  //   this.innerHTML = "&times;";
+  document
+    .getElementsByClassName("menu__link")[0]
+    .classList.toggle("show__menu__mobile");
+  document
+    .getElementsByClassName("menu")[0]
+    .classList.toggle("menu__height__auto");
+};
+var control = document.getElementsByClassName("control-panel")[0];
+document.getElementById("filter").onclick = function () {
+  if (control.style.display == "grid") {
+    document.getElementsByClassName("control-panel")[0].style.display = "none";
+  } else {
+    document.getElementsByClassName("control-panel")[0].style.display = "grid";
+  }
+};
